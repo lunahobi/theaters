b1c16403bc678983b64786281b8841b9169ed644
lunahobi <muradyan.lusine.05@mail.ru>
2024-01-05 19:39:31 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Театральные площадки и коллективы</title>
+    <link rel="shortcut icon" href="img/logo.svg" type="image/x-icon" />
+    <meta name="description" content="Сведения о Театральных учреждениях, занесенных в информационную систему с описанием деятельности учреждения, его расположения, контактных данных, сайта учреждения и времени его работы.">
+
+    <!-- подключение стилей gooleFonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;1,600&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        
+
+    </header>
+
+    <!-- Bootstrap JS и зависимости -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
