7ba1f96d2eb105d012bbffc54a95b0b3103a5089
lunahobi <muradyan.lusine.05@mail.ru>
2024-01-08 00:12:15 +0500

    Сделаны хэдер и футер с обратной связью

--- index.php
+++ index.php
@@ -15,16 +15,103 @@
 
     <!-- Bootstrap CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
 </head>
 
 <body>
-    <header>
-        
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top d-flex flex-wrap">
+        <div class="container">
+            <a class="navbar-brand d-flex me-md-auto" href="index.php">Театры России</a>
 
-    </header>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mb-2 mb-md-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.php">Главная</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">О нас</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Карта</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Авторизация</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
+    <main class="container">
+        <div class="bg-body-tertiary p-5 rounded">
+            <h1>Navbar example</h1>
+            <p class="lead">This example is a quick exercise to illustrate how fixed to top navbar works. As you scroll, it will remain fixed to the top of your browser’s viewport.</p>
+            <a class="btn btn-lg btn-primary" href="../components/navbar/" role="button">View navbar docs &raquo;</a>
+        </div>
+    </main>
+
+    <!-- Форма обратной связи и контакты -->
+    <section id='contacts' class="py-5">
+        <div class="container mw-80">
+            <div class="row">
+                <h2 class="text-center">Контакты</h2>
+                <div class="col-1"></div>
+                <div class="col">
+                    <form>
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Имя</label>
+                            <input type="text" class="form-control" id="name" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="message" class="form-label">Сообщение</label>
+                            <textarea class="form-control" id="message" rows="4" required></textarea>
+                        </div>
+                        <div class="text-center">
+                            <button id='btn-contacts'type="submit" class="btn">Отправить</button>
+                        </div>
+
+                    </form>
+                </div>
+                <div class="col py-3">
+                    <div class="hstack gap-2 mb-1">
+                        <img src="img/email.svg" alt="e-mail">
+                        muradyan.lusine.05@mail.ru
+                    </div>
+
+                    <div class="hstack gap-2 mb-2">
+                        <img src="img/telephone.svg" alt="telephone">
+                        +7 (967) 382-30-10
+                    </div>
+
+                    <div class="hstack gap-2 ">
+                        <img src="img/telegram.svg" alt="telegram">
+                        <img src="img/instagram.svg" alt="instagram">
+                        <img src="img/facebook.svg" alt="facebook">
+                        <img src="img/whatsapp.svg" alt="whatsapp">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="text-center py-1">
+        Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved.<br>
+        Для создания приложения были использованы открытые данные <a href="https://opendata.mkrf.ru/">Министерства культуры РФ</a>:<br>
+        <a href="https://opendata.mkrf.ru/opendata/7705851331-theaters">Театральные площадки и коллективы</a>
+    </footer>
     <!-- Bootstrap JS и зависимости -->
     <script src="js/bootstrap.bundle.min.js"></script>
+
+
 </body>
 
 </html>
