66a1a5bd776e6e3bec4ee4744c52c18c601da8c8
lunahobi <muradyan.lusine.05@mail.ru>
2024-01-12 01:09:25 +0500

    Реализовала отравку данных с формы обратной связи на бд

--- index.php
+++ index.php
@@ -18,6 +18,11 @@
 
     <!-- Custom styles for this template -->
     <link href="css/style.css" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="js/jquery.maskedinput.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/script.js"></script>
 </head>
 
 <body>
@@ -60,7 +65,7 @@
                         <div class="carousel-caption text-start">
                             <h1 class='text-carousel-h1 d-none d-md-block'>Добро пожаловать на сайт с театральными плошадками и коллективами РФ!</h1>
                             <h1 class='text-carousel-h1 d-block d-md-none'>Добро пожаловать!</h1>
-                            <p class='text-carousel-p d-none d-md-block' >Здесь вы сможете выбрать театр поблизости либо по различным критериям, также у вас есть возможность просматировать их на карте, а еще прокладывать к ним путь</p>
+                            <p class='text-carousel-p d-none d-md-block'>Здесь вы сможете выбрать театр поблизости либо по различным критериям, также у вас есть возможность просматировать их на карте, а еще прокладывать к ним путь</p>
                             <p class='text-carousel-p d-block d-md-none'>Здесь вы сможете выбрать театр, также у вас есть возможность просматировать их на карте, а еще прокладывать к ним путь</p>
                             <p><a class="btn btn-lg btn-carousel" href="#info">Подробнее</a></p>
                         </div>
@@ -148,7 +153,7 @@
             <div class="row">
                 <h2 class="text-center">Контакты</h2>
                 <div class="col">
-                    <form action="send.php" method="POST">
+                    <form action="" method="POST">
                         <div class="mb-3">
                             <label for="name" class="form-label">Имя</label>
                             <input type="text" class="form-control" id="name" name="name" required>
@@ -163,9 +168,19 @@
                         </div>
                         <div class="text-center">
                             <button id='btn-contacts' type="submit" class="btn">Отправить</button>
+                            <div class="status"></div>
                         </div>
-
                     </form>
+                    <?php
+
+                    include "db.php";
+                    if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
+                        $query = "INSERT INTO `messages` (`name`, `email`, `message`) VALUES ('{$_POST['name']}', '{$_POST['email']}', '{$_POST['message']}')";
+
+                        $result = mysqli_query($mysql, $query);
+                    }
+                    ?>
+
                 </div>
                 <div class="col py-3">
                     <div class="hstack gap-2 mb-1">
