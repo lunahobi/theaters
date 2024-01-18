ce03055bef03cf322857bf547108428116e8037b
lunahobi <muradyan.lusine.05@mail.ru>
2024-01-18 23:18:33 +0500

    Доработана обратная связь

--- actions/feedback.php
+++ actions/feedback.php
@@ -1,9 +1,13 @@
 <?php
 
 include "mysql/Theaters_DB_Access.php";
+
 if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $msg = $_POST['message'];
     $conn = new Theater_DB_Access;
     $conn->prepare_query("INSERT INTO `messages` (`name`, `email`, `message`) VALUES (?, ?, ?)");
-    $conn->issue_query(array($_POST['name'], $_POST['email'], $_POST['message']));
+    $conn->issue_query(array($name, $email, $msg));
 }
 header('Location: /');
